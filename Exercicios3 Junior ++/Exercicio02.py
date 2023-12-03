3f6cf5a72dc41a221c0fcfc0518139e60d3c8d6b
Henrique Hastenreiter <109881935+Kiteyy7@users.noreply.github.com>
2023-12-03 08:37:37 -0300

    Add files via upload

---
+++ Exercicios3 Junior ++/Exercicio02.py
@@ -0,0 +1,12 @@
+nome = input("Digite seu nome: ")
+senha = input("Digite sua senha: ")
+
+
+while nome == senha:
+    print('Seja mais criativo em seu login')
+    nome = input("Digite seu nome: ")
+    senha = input("Digite sua senha: ")
+
+else: 
+    print('Voce foi cadastrado')
+
---
+++ Exercicios3 Junior ++/Exercicio03.py
@@ -0,0 +1,7 @@
+nome = float(input("Digite seu Nome: "))
+
+
+while nome <= 3:
+    print('Seu nome necessita de pelo menos 3 caracteres')
+
+ 
